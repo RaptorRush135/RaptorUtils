68114da6dac208036454cc3a423c3082d3ce02c7
RaptorRush <raptorrush135@hotmail.com>
2024-10-27 22:51:35 -0600

    Add Array and List Any extension

---
+++ Core/Collections/Extensions/ArrayExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace RaptorUtils.Collections.Extensions;
+
+/// <summary>
+/// Provides extension methods for arrays.
+/// </summary>
+public static class ArrayExtensions
+{
+    /// <summary>
+    /// Determines whether any element of the specified array satisfies a condition.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the array.</typeparam>
+    /// <param name="array">The array to check against the condition.</param>
+    /// <param name="predicate">
+    /// A function to test each element for a condition. The element will satisfy the condition if
+    /// this function returns <see langword="true"/>.
+    /// </param>
+    /// <returns>
+    /// <see langword="true"/> if any elements in the list match the specified condition;
+    /// otherwise, <see langword="false"/>.
+    /// </returns>
+    public static bool Any<T>(this T[] array, Func<T, bool> predicate)
+    {
+        return Array.Exists(array, new Predicate<T>(predicate));
+    }
+}
---
+++ Core/Collections/Extensions/ListExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace RaptorUtils.Collections.Extensions;
+
+/// <summary>
+/// Provides extension methods for lists.
+/// </summary>
+public static class ListExtensions
+{
+    /// <summary>
+    /// Determines whether any element of the specified list satisfies a condition.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the list.</typeparam>
+    /// <param name="list">The list to check against the condition.</param>
+    /// <param name="predicate">
+    /// A function to test each element for a condition. The element will satisfy the condition if
+    /// this function returns <see langword="true"/>.
+    /// </param>
+    /// <returns>
+    /// <see langword="true"/> if any elements in the list match the specified condition;
+    /// otherwise, <see langword="false"/>.
+    /// </returns>
+    public static bool Any<T>(this List<T> list, Func<T, bool> predicate)
+    {
+        return list.Exists(new Predicate<T>(predicate));
+    }
+}
