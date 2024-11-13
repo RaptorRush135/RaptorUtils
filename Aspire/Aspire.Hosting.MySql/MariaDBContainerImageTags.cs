caa3025b10c9f02e6765a480308225ae698dc0a6
RaptorRush <raptorrush135@hotmail.com>
2024-11-13 01:10:36 -0600

    Add RaptorUtils.Aspire.Hosting.MySql

---
+++ Aspire/Aspire.Hosting.MySql/MariaDBContainerImageTags.cs
@@ -0,0 +1,16 @@
+﻿namespace RaptorUtils.Aspire.Hosting.MySql;
+
+/// <summary>
+/// Contains constant values related to the MariaDB Docker container image.
+/// </summary>
+internal static class MariaDBContainerImageTags
+{
+    /// <summary>docker.io.</summary>
+    public const string Registry = "docker.io";
+
+    /// <summary>library/mariadb.</summary>
+    public const string Image = "library/mariadb";
+
+    /// <summary>11.5.2.</summary>
+    public const string Tag = "11.5.2";
+}
