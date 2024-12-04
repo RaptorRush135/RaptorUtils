024cbcd98a36e18b75c13d32524882262585696b
RaptorRush <raptorrush135@hotmail.com>
2024-12-03 19:32:10 -0600

    CollectionExtensions: Add RemoveAll

--- Core/Collections/Extensions/CollectionExtensions.cs
+++ Core/Collections/Extensions/CollectionExtensions.cs
@@ -36,4 +36,35 @@ public static class CollectionExtensions
             destination.Add(item);
         }
     }
+
+    /// <summary>
+    /// Removes all elements from the specified <see cref="ICollection{T}"/>
+    /// that match the conditions defined by the provided predicate.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the collection.</typeparam>
+    /// <param name="destination">The collection from which elements will be removed.</param>
+    /// <param name="predicate">
+    /// A function that defines the conditions of the elements to remove.
+    /// The function should return <see langword="true"/> for elements that should be removed,
+    /// and <see langword="false"/> otherwise.
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown if <paramref name="destination"/> or <paramref name="predicate"/> is <see langword="null"/>.
+    /// </exception>
+    public static void RemoveAll<T>(this ICollection<T> destination, Func<T, bool> predicate)
+    {
+        ArgumentNullException.ThrowIfNull(destination);
+        ArgumentNullException.ThrowIfNull(predicate);
+
+        if (destination is List<T> list)
+        {
+            list.RemoveAll(new Predicate<T>(predicate));
+            return;
+        }
+
+        foreach (T item in destination.Where(predicate).ToList())
+        {
+            destination.Remove(item);
+        }
+    }
 }
