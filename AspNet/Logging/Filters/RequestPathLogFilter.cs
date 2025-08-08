fa562b6711eb269eff6b612fc8935d338ddf6183
RaptorRush135 <112044769+RaptorRush135@users.noreply.github.com>
2025-08-07 20:51:02 -0600

    feat: Enhance RequestPathLogFilter with minimum log level support

--- AspNet/Logging/Filters/RequestPathLogFilter.cs
+++ AspNet/Logging/Filters/RequestPathLogFilter.cs
@@ -4,16 +4,27 @@ using Serilog.Core;
 using Serilog.Events;
 
 /// <summary>
-/// A filter that determines whether a log event should be logged based on the request path.
+/// A filter that allows log events to be processed only if the request path associated with the event
+/// matches a given predicate, or if their level is greater than or equal to a specified minimum level.
 /// </summary>
-/// <param name="pathFilter">A predicate function to evaluate the request path.</param>
+/// <param name="pathFilter">
+/// A predicate function used to evaluate the request path (from the log event's "RequestPath" property).
+/// If the log event's level is below the minimum level, this predicate determines whether the event is logged.
+/// </param>
+/// <param name="minimumLevel">
+/// The minimum log level required for a log event to be automatically logged regardless of its request path.
+/// </param>
 public class RequestPathLogFilter(
-    Predicate<string> pathFilter)
+    Predicate<string> pathFilter,
+    LogEventLevel minimumLevel = LogEventLevel.Warning)
     : ILogEventFilter
 {
     /// <summary>
-    /// Determines if the specified log event should be logged based on the request path filter.
-    /// If the log event does not contain a request path, it is considered enabled.
+    /// Determines whether the specified log event should be logged.
+    /// Events at or above the configured minimum level are always logged.
+    /// Events below the minimum level are logged only if their "RequestPath" property
+    /// satisfies the provided path predicate.
+    /// If the "RequestPath" property is missing, the event is logged by default.
     /// </summary>
     /// <param name="logEvent">The log event to evaluate.</param>
     /// <returns>
@@ -21,7 +32,7 @@ public class RequestPathLogFilter(
     /// </returns>
     public bool IsEnabled(LogEvent logEvent)
     {
-        if (logEvent.Level >= LogEventLevel.Warning // TODO
+        if (logEvent.Level >= minimumLevel
             || !logEvent.Properties.TryGetValue("RequestPath", out var path))
         {
             return true;
--- AspNet/Logging/Filters/RequestPathPrefixLogFilter.cs
+++ AspNet/Logging/Filters/RequestPathPrefixLogFilter.cs
@@ -1,13 +1,22 @@
 ﻿namespace RaptorUtils.AspNet.Logging.Filters;
 
+using Serilog.Events;
+
 /// <summary>
-/// A log filter that enables logging for events where the request path starts with a specified prefix.
+/// A log filter that enables logging for events where the request path starts with a specified prefix,
+/// or the log level is greater than or equal to a specified minimum.
 /// </summary>
 /// <param name="prefix">The prefix that the request path must start with to enable logging.</param>
 /// <param name="comparison">
-/// The string comparison option to use when checking the prefix (default is <see cref="StringComparison.InvariantCulture"/>).
+/// The string comparison option to use when checking the prefix.
+/// </param>
+/// <param name="minimumLevel">
+/// The minimum log level at which events are always logged, regardless of their request path.
 /// </param>
 public class RequestPathPrefixLogFilter(
     string prefix,
-    StringComparison comparison = StringComparison.InvariantCulture)
-    : RequestPathLogFilter(path => path.StartsWith($"\"{prefix}", comparison));
+    StringComparison comparison = StringComparison.InvariantCulture,
+    LogEventLevel minimumLevel = LogEventLevel.Warning)
+    : RequestPathLogFilter(
+        path => path.StartsWith($"\"{prefix}", comparison),
+        minimumLevel);
