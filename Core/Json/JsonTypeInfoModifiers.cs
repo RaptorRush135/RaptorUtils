fe16143624967fcf00e366893b8699ebbd668da9
RaptorRush135 <112044769+RaptorRush135@users.noreply.github.com>
2025-09-10 01:53:22 -0600

    feat(Core): Add 'MakeAllPropertiesOptional' (JsonTypeInfoModifier)

---
+++ Core/Json/JsonTypeInfoModifiers.cs
@@ -0,0 +1,25 @@
+﻿namespace RaptorUtils.Json;
+
+using System.Text.Json.Serialization.Metadata;
+
+/// <summary>
+/// Provides helpers for modifying <see cref="JsonTypeInfo"/> instances
+/// used in <see cref="System.Text.Json"/> serialization.
+/// </summary>
+public static class JsonTypeInfoModifiers
+{
+    /// <summary>
+    /// Marks all properties in the specified <see cref="JsonTypeInfo"/>
+    /// as optional by setting <see cref="JsonPropertyInfo.IsRequired"/> to <see langword="false"/>.
+    /// </summary>
+    /// <param name="typeInfo">
+    /// The <see cref="JsonTypeInfo"/> whose property metadata will be modified.
+    /// </param>
+    public static void MakeAllPropertiesOptional(JsonTypeInfo typeInfo)
+    {
+        foreach (var property in typeInfo.Properties)
+        {
+            property.IsRequired = false;
+        }
+    }
+}
