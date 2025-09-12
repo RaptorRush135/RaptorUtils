bfef9d685f578b4a7977c56a3aff8bebad230069
RaptorRush135 <112044769+RaptorRush135@users.noreply.github.com>
2025-09-12 01:26:39 -0600

    feat(EntityFramework): Add ModelBuilder Extensions

---
+++ AspNet/EntityFramework/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace RaptorUtils.AspNet.EntityFramework.Extensions;
+
+using Microsoft.EntityFrameworkCore;
+
+using RaptorUtils.EntityFramework.Metadata.Extensions;
+using RaptorUtils.Extensions;
+
+/// <summary>
+/// Provides extension methods for <see cref="ModelBuilder"/>.
+/// </summary>
+public static class ModelBuilderExtensions
+{
+    /// <summary>
+    /// Singularizes the table names of ASP.NET Identity entities by removing a trailing 's' from tables
+    /// whose names start with "AspNet".
+    /// </summary>
+    /// <param name="builder">The <see cref="ModelBuilder"/> instance to apply the configuration to.</param>
+    public static void SingularizeAspNetIdentityTables(this ModelBuilder builder)
+    {
+        builder.Model.GetEntityTypes()
+            .Where(t => t.GetTableName()?.StartsWith("AspNet") ?? false)
+                .Configure(
+                    e => e.SetTableName(
+                        e.GetTableName()!.RemoveRequiredSuffix("s", StringComparison.InvariantCulture)));
+    }
+}
---
+++ EntityFramework/Metadata/Extensions/EntityFrameworkMetadataExtensions.cs
@@ -0,0 +1,47 @@
+﻿namespace RaptorUtils.EntityFramework.Metadata.Extensions;
+
+using Microsoft.EntityFrameworkCore.Metadata;
+
+/// <summary>
+/// Provides extension methods for entity/property configuration.
+/// </summary>
+public static class EntityFrameworkMetadataExtensions
+{
+    /// <summary>
+    /// Applies the specified <paramref name="convention"/> action to each entity type in the collection.
+    /// </summary>
+    /// <param name="entityTypes">
+    /// The collection of entity types to configure.
+    /// </param>
+    /// <param name="convention">
+    /// An action that applies a convention to an individual <see cref="IMutableEntityType"/>.
+    /// </param>
+    public static void Configure(
+        this IEnumerable<IMutableEntityType> entityTypes,
+        Action<IMutableEntityType> convention)
+    {
+        foreach (var entityType in entityTypes)
+        {
+            convention(entityType);
+        }
+    }
+
+    /// <summary>
+    /// Applies the specified <paramref name="convention"/> action to each property in the collection.
+    /// </summary>
+    /// <param name="propertyTypes">
+    /// The collection of properties to configure.
+    /// </param>
+    /// <param name="convention">
+    /// An action that applies a convention to an individual <see cref="IMutableProperty"/>.
+    /// </param>
+    public static void Configure(
+        this IEnumerable<IMutableProperty> propertyTypes,
+        Action<IMutableProperty> convention)
+    {
+        foreach (var propertyType in propertyTypes)
+        {
+            convention(propertyType);
+        }
+    }
+}
