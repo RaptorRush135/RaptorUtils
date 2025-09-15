6e79ebc8d786de41f6c094c631f96931308aac5f
RaptorRush135 <112044769+RaptorRush135@users.noreply.github.com>
2025-09-14 18:38:51 -0600

    feat(Core): Add IIdentifiable

---
+++ Core/Common/IIdentifiable{T}.cs
@@ -0,0 +1,15 @@
+﻿namespace RaptorUtils.Common;
+
+/// <summary>
+/// Represents an object that has a unique identifier of type <typeparamref name="T"/>.
+/// </summary>
+/// <typeparam name="T">
+/// The type of the identifier.
+/// </typeparam>
+public interface IIdentifiable<out T>
+{
+    /// <summary>
+    /// Gets the unique identifier of this object.
+    /// </summary>
+    T Id { get; }
+}
