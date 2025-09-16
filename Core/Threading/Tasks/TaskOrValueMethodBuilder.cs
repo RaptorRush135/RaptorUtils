5c73cf2dcb4422ba3c61304eb97f105daf302a7b
RaptorRush135 <112044769+RaptorRush135@users.noreply.github.com>
2025-09-15 18:54:27 -0600

    feat(Core): Add TaskOrValue

---
+++ Core/Threading/Tasks/TaskOrValueMethodBuilder.cs
@@ -0,0 +1,84 @@
+﻿namespace RaptorUtils.Threading.Tasks;
+
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+/// <summary>
+/// Represents a builder for asynchronous methods that returns a <see cref="TaskOrValue{TResult}"/>.
+/// </summary>
+/// <typeparam name="TResult">The type of the result.</typeparam>
+[StructLayout(LayoutKind.Auto)]
+public struct TaskOrValueMethodBuilder<TResult>
+{
+    private AsyncValueTaskMethodBuilder<TResult> builder;
+
+    /// <summary>
+    /// Gets the value task for this builder.
+    /// </summary>
+    public TaskOrValue<TResult> Task => new(this.builder.Task);
+
+    /// <summary>
+    /// Creates an instance of the <see cref="TaskOrValueMethodBuilder{TResult}"/> struct.
+    /// </summary>
+    /// <returns>The initialized instance.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static TaskOrValueMethodBuilder<TResult> Create() => default;
+
+    /// <summary>Begins running the builder with the associated state machine.</summary>
+    /// <typeparam name="TStateMachine">The type of the state machine.</typeparam>
+    /// <param name="stateMachine">The state machine instance, passed by reference.</param>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Start<TStateMachine>(ref TStateMachine stateMachine)
+        where TStateMachine : IAsyncStateMachine =>
+        this.builder.Start(ref stateMachine);
+
+    /// <summary>Associates the builder with the specified state machine.</summary>
+    /// <param name="stateMachine">The state machine instance to associate with the builder.</param>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void SetStateMachine(IAsyncStateMachine stateMachine)
+        => this.builder.SetStateMachine(stateMachine);
+
+    /// <summary>Marks the value task as successfully completed.</summary>
+    /// <param name="result">The result to use to complete the value task.</param>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void SetResult(TResult result)
+        => this.builder.SetResult(result);
+
+    /// <summary>
+    /// Marks the value task as failed and binds the specified exception to the value task.
+    /// </summary>
+    /// <param name="exception">The exception to bind to the value task.</param>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void SetException(Exception exception)
+        => this.builder.SetException(exception);
+
+    /// <summary>
+    /// Schedules the state machine to proceed to the next action when the specified awaiter completes.
+    /// </summary>
+    /// <typeparam name="TAwaiter">The type of the awaiter.</typeparam>
+    /// <typeparam name="TStateMachine">The type of the state machine.</typeparam>
+    /// <param name="awaiter">the awaiter.</param>
+    /// <param name="stateMachine">The state machine.</param>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void AwaitOnCompleted<TAwaiter, TStateMachine>(
+        ref TAwaiter awaiter,
+        ref TStateMachine stateMachine)
+        where TAwaiter : INotifyCompletion
+        where TStateMachine : IAsyncStateMachine
+        => this.builder.AwaitOnCompleted(ref awaiter, ref stateMachine);
+
+    /// <summary>
+    /// Schedules the state machine to proceed to the next action when the specified awaiter completes.
+    /// </summary>
+    /// <typeparam name="TAwaiter">The type of the awaiter.</typeparam>
+    /// <typeparam name="TStateMachine">The type of the state machine.</typeparam>
+    /// <param name="awaiter">the awaiter.</param>
+    /// <param name="stateMachine">The state machine.</param>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void AwaitUnsafeOnCompleted<TAwaiter, TStateMachine>(
+        ref TAwaiter awaiter,
+        ref TStateMachine stateMachine)
+        where TAwaiter : ICriticalNotifyCompletion
+        where TStateMachine : IAsyncStateMachine
+        => this.builder.AwaitUnsafeOnCompleted(ref awaiter, ref stateMachine);
+}
---
+++ Core/Threading/Tasks/TaskOrValue{TResult}.cs
@@ -0,0 +1,80 @@
+﻿namespace RaptorUtils.Threading.Tasks;
+
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+/// <summary>
+/// Represents a value that may be available synchronously or asynchronously as a <see cref="ValueTask{TResult}"/>,
+/// and can be implicitly created from a <typeparamref name="TResult"/> value or a <see cref="ValueTask{TResult}"/>.
+/// </summary>
+/// <typeparam name="TResult">The type of the result.</typeparam>
+[AsyncMethodBuilder(typeof(TaskOrValueMethodBuilder<>))]
+[method: MethodImpl(MethodImplOptions.AggressiveInlining)]
+public readonly struct TaskOrValue<TResult>(
+    ValueTask<TResult> task)
+    : IEquatable<TaskOrValue<TResult>>
+{
+    /// <summary>
+    /// Gets the underlying <see cref="ValueTask{TResult}"/>.
+    /// </summary>
+    public ValueTask<TResult> UnderlyingTask { get; } = task;
+
+    public static implicit operator TaskOrValue<TResult>(TResult value)
+        => new(ValueTask.FromResult(value));
+
+    public static implicit operator TaskOrValue<TResult>(ValueTask<TResult> task)
+        => new(task);
+
+    public static bool operator ==(TaskOrValue<TResult> left, TaskOrValue<TResult> right)
+        => left.UnderlyingTask == right.UnderlyingTask;
+
+    public static bool operator !=(TaskOrValue<TResult> left, TaskOrValue<TResult> right)
+        => left.UnderlyingTask != right.UnderlyingTask;
+
+    /// <summary>
+    /// Gets an awaiter for this <see cref="TaskOrValue{TResult}"/>.
+    /// </summary>
+    /// <returns>The awaiter.</returns>
+    public ValueTaskAwaiter<TResult> GetAwaiter() => this.UnderlyingTask.GetAwaiter();
+
+    /// <summary>
+    /// Returns the hash code for this instance.
+    /// </summary>
+    /// <returns>The hash code.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override int GetHashCode() => this.UnderlyingTask.GetHashCode();
+
+    /// <summary>
+    /// Determines whether this instance and a specified object are equal.
+    /// </summary>
+    /// <param name="obj">The object to compare with this instance.</param>
+    /// <returns>
+    /// <see langword="true"/> if <paramref name="obj"/> is a <see cref="TaskOrValue{TResult}"/> and equal;
+    /// otherwise, <see langword="false"/>.
+    /// </returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override bool Equals([NotNullWhen(true)] object? obj) => this.UnderlyingTask.Equals(obj);
+
+    /// <summary>
+    /// Determines whether this instance and a specified <see cref="TaskOrValue{TResult}"/> are equal.
+    /// </summary>
+    /// <param name="other">The other <see cref="TaskOrValue{TResult}"/> to compare.</param>
+    /// <returns><see langword="true"/> if the two are equal; otherwise, <see langword="false"/>.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public bool Equals(TaskOrValue<TResult> other) => this.UnderlyingTask.Equals(other.UnderlyingTask);
+
+    /// <summary>
+    /// Gets a <see cref="Task{TResult}"/> object to represent this ValueTask.
+    /// </summary>
+    /// <returns>
+    /// It will either return the wrapped task object if one exists, or it'll
+    /// manufacture a new task object to represent the result.
+    /// </returns>
+    public Task<TResult> AsTask() => this.UnderlyingTask.AsTask();
+
+    /// <summary>
+    /// Gets a <see cref="ValueTask{TResult}"/> that may be used at any point in the future.
+    /// </summary>
+    /// <returns>The <see cref="ValueTask{TResult}"/>.</returns>
+    public ValueTask<TResult> Preserve() => this.UnderlyingTask.Preserve();
+}
