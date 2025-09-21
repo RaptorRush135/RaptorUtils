1112be2944119490b8e12fc01e1b0d94b00e6503
RaptorRush135 <112044769+RaptorRush135@users.noreply.github.com>
2025-09-20 21:28:03 -0600

    feat: Add FluentValidation project. Add Unique items validator

---
+++ FluentValidation/CustomValidatorExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace RaptorUtils.FluentValidation;
+
+using global::FluentValidation;
+
+using RaptorUtils.FluentValidation.Validators;
+
+/// <summary>
+/// Provides extension methods for FluentValidation rule builders.
+/// </summary>
+public static class CustomValidatorExtensions
+{
+    /// <summary>
+    /// This extension applies the UniqueItemsValidator to ensure no duplicate items exist in the collection.
+    /// </summary>
+    /// <typeparam name="T">The type of the object being validated that contains the collection property.</typeparam>
+    /// <typeparam name="TElement">The type of elements contained within the collection being validated.</typeparam>
+    /// <param name="ruleBuilder">The rule builder on which the validator should be defined.</param>
+    /// <returns>
+    /// An IRuleBuilderOptions instance that allows for further configuration of the validation rule.
+    /// </returns>
+    public static IRuleBuilderOptions<T, IEnumerable<TElement>> MustHaveUniqueItems<T, TElement>(
+        this IRuleBuilder<T, IEnumerable<TElement>> ruleBuilder)
+    {
+        return ruleBuilder.SetValidator(new UniqueItemsValidator<T, TElement>());
+    }
+}
---
+++ FluentValidation/Validators/UniqueItemsValidator{T,TElement}.cs
@@ -0,0 +1,32 @@
+﻿namespace RaptorUtils.FluentValidation.Validators;
+
+using global::FluentValidation;
+using global::FluentValidation.Validators;
+
+using RaptorUtils.Collections.Extensions;
+
+/// <summary>
+/// A FluentValidation property validator that ensures all elements in a collection are unique.
+/// </summary>
+/// <typeparam name="T">The type of the object being validated that contains the collection property.</typeparam>
+/// <typeparam name="TElement">The type of elements contained within the collection being validated.</typeparam>
+public class UniqueItemsValidator<T, TElement>
+    : PropertyValidator<T, IEnumerable<TElement>>
+{
+    /// <summary>
+    /// Gets the name of the validator.
+    /// This is used as the default Error Code for the validator.
+    /// </summary>
+    public override string Name => nameof(UniqueItemsValidator<T, TElement>);
+
+    /// <inheritdoc/>
+    public override bool IsValid(ValidationContext<T> context, IEnumerable<TElement> collection)
+        => !collection.ContainsDuplicates();
+
+    /// <summary>
+    /// Returns the default error message template for this validator.
+    /// </summary>
+    /// <inheritdoc/>
+    protected override string GetDefaultMessageTemplate(string errorCode)
+        => "'{PropertyName}' must contain only unique elements.";
+}
