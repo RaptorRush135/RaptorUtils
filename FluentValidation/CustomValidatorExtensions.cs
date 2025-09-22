e72fb738f6e7936ef8564bbd00e2dd0a33eeac04
RaptorRush135 <112044769+RaptorRush135@users.noreply.github.com>
2025-09-22 00:06:38 -0600

    feat(FluentValidation): Add EmailAddressWithMaxLength validation

--- FluentValidation/CustomValidatorExtensions.cs
+++ FluentValidation/CustomValidatorExtensions.cs
@@ -5,10 +5,30 @@ using global::FluentValidation;
 using RaptorUtils.FluentValidation.Validators;
 
 /// <summary>
-/// Provides extension methods for FluentValidation rule builders.
+/// Extension methods that provide a custom set of validators.
 /// </summary>
 public static class CustomValidatorExtensions
 {
+    /// <summary>
+    /// Defines the maximum valid length for an email address according to RFC standards.
+    /// </summary>
+    public const int EmailMaxValidLength = 254;
+
+    /// <summary>
+    /// Applies email address validation along with a maximum length check.
+    /// </summary>
+    /// <typeparam name="T">The type of the object being validated.</typeparam>
+    /// <param name="ruleBuilder">The rule builder on which the validator should be defined.</param>
+    /// <returns>
+    /// An IRuleBuilderOptions instance that allows for further configuration of the validation rule.
+    /// </returns>
+    public static IRuleBuilderOptions<T, string> EmailAddressWithMaxLength<T>(
+        this IRuleBuilder<T, string> ruleBuilder)
+    {
+        return ruleBuilder.EmailAddress()
+            .MaximumLength(EmailMaxValidLength);
+    }
+
     /// <summary>
     /// This extension applies the UniqueItemsValidator to ensure no duplicate items exist in the collection.
     /// </summary>
