244adf96aa812f38d831b548fdbd23bf1be11143
RaptorRush135 <112044769+RaptorRush135@users.noreply.github.com>
2026-02-08 04:09:35 -0600

    feat: add ServiceCollectionServiceExtensions

---
+++ Core/Extensions/DependencyInjection/ServiceCollectionServiceExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace RaptorUtils.Extensions.DependencyInjection;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using MsExtensions = Microsoft.Extensions.DependencyInjection.ServiceCollectionServiceExtensions;
+
+/// <summary>
+/// <inheritdoc cref="MsExtensions" />
+/// </summary>
+public static class ServiceCollectionServiceExtensions
+{
+    /// <summary>
+    /// Adds a service of the type specified in <typeparamref name="TService"/> with an
+    /// implementation type specified in <typeparamref name="TImplementation"/> to the
+    /// specified <see cref="IServiceCollection"/>.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add the service to.</param>
+    /// <param name="lifetime">The <see cref="ServiceLifetime"/> of the service.</param>
+    /// <inheritdoc cref="MsExtensions.AddScoped{TService, TImplementation}(IServiceCollection)" />
+    public static IServiceCollection Add<TService, TImplementation>(
+        this IServiceCollection services,
+        ServiceLifetime lifetime)
+        where TService : class
+        where TImplementation : class, TService
+    {
+        ArgumentNullException.ThrowIfNull(services);
+
+        var descriptor = new ServiceDescriptor(typeof(TService), typeof(TImplementation), lifetime);
+        services.Add(descriptor);
+
+        return services;
+    }
+}
